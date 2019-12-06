251f8d99a4b44683f281d7fa2103417292d5adb0
Roberto Oliveira <robertooliveiragp@gmail.com>
2019-12-06 19:32:17 -0300

    backend: create Player controller

--- backend/src/routes.js
+++ backend/src/routes.js
@@ -6,6 +6,7 @@ const authMiddleware = require('./app/middlewares/auth');
 const UserController = require('./app/controllers/UserController');
 const SessionController = require('./app/controllers/SessionController');
 const PositionController = require('./app/controllers/PositionController');
+const PlayerController = require('./app/controllers/PlayerController');
 
 routes.get('/users', UserController.index);
 routes.get('/users/:id', UserController.show);
@@ -14,12 +15,20 @@ routes.delete('/users/:id', UserController.destroy);
 
 routes.post('/sessions', SessionController.store);
 //routes.use(authMiddleware);
-routes.get('/dashboard', (req, res) => { ok: true });
+routes.get('/dashboard', (req, res) => {
+    return res.json({ ok: true });
+});
 
 routes.post('/positions', PositionController.store);
 routes.get('/positions', PositionController.index);
 routes.get('/positions/:id', PositionController.show);
 routes.put('/positions/:id', PositionController.update);
 routes.delete('/positions/:id', PositionController.destroy);
+
+routes.post('/players', PlayerController.store);
+routes.get('/players', PlayerController.index);
+routes.get('/players/:id', PlayerController.show);
+routes.put('/players/:id', PlayerController.update);
+routes.delete('/players/:id', PlayerController.destroy);
 
 module.exports = routes;
