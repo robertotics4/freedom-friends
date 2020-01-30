f2f78c7d459663243f164099a8f2cffd8ce25dae
Roberto Oliveira <47399431+robertotics4@users.noreply.github.com>
2020-01-30 15:58:21 -0300

    backend: adjustment in positionController

--- backend/src/app/controllers/PositionController.js
+++ backend/src/app/controllers/PositionController.js
@@ -84,27 +84,4 @@ module.exports = {
             return res.status(500).json({ error: err.message });
         }
     },
-
-    async update(req, res) {
-        try {
-            const { id } = req.params;
-            const { name, initials } = req.body;
-
-            let position = await Position.findByPk(id);
-
-            if (!position)
-                return res.status(404).json({ msg: 'Position not found' });
-
-            positionFormatted = {
-                name: name.toUpperCase(),
-                initials: initials.trim().toUpperCase(),
-            }
-
-            position = await Position.update(positionFormatted, { where: { id } });
-
-            return res.status(200).json(positionFormatted);
-        } catch (err) {
-            return res.status(500).json({ error: err.message });
-        }
-    },
 };
